524118e53a6ecf9dcc9cf9763782fab68c7ec8ec
d.mathisen <drew.a.mathisen@gmail.com>
2021-12-19 15:30:03 -0700

    figured out text wrapping for PDF

--- src/views/Pdf.vue
+++ src/views/Pdf.vue
@@ -79,22 +79,28 @@ export default {
     download() {
       let pdfName = "test";
       var doc = new jsPDF();
+      var loremipsum =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id eros turpis. Vivamus tempor urna vitae sapien mollis molestie. Vestibulum in lectus non enim bibendum laoreet at at libero. Etiam malesuada erat sed sem blandit in varius orci porttitor. Sed at sapien urna. Fusce augue ipsum, molestie et adipiscing at, varius quis enim. Morbi sed magna est, vel vestibulum urna. Sed tempor ipsum vel mi pretium at elementum urna tempor. Nulla faucibus consectetur felis, elementum venenatis mi mollis gravida. Aliquam mi ante, accumsan eu tempus vitae, viverra quis justo.\n\nProin feugiat augue in augue rhoncus eu cursus tellus laoreet. Pellentesque eu sapien at diam porttitor venenatis nec vitae velit. Donec ultrices volutpat lectus eget vehicula. Nam eu erat mi, in pulvinar eros. Mauris viverra porta orci, et vehicula lectus sagittis id. Nullam at magna vitae nunc fringilla posuere. Duis volutpat malesuada ornare. Nulla in eros metus. Vivamus a posuere libero.";
 
       doc.setFontSize(20);
       doc.text("Title: " + this.entry.title, 107.95, 20, "center");
 
       doc.setFontSize(10);
       doc.text("Start time: " + this.entry.start_time, 20, 30, "left");
-      doc.text("End time: " + this.entry.end_time, 20, 40, "left");
-      doc.text("Date: " + this.entry.date, 20, 50, "left");
-      doc.text("Location: " + this.entry.location, 20, 60, "left");
-      doc.text("Seeing Conditions: " + this.entry.seeing_conditions, 20, 70, "left");
+      doc.text("End time: " + this.entry.end_time, 20, 35, "left");
+      doc.text("Date: " + this.entry.date, 20, 40, "left");
+      doc.text("Location: " + this.entry.location, 20, 45, "left");
+      doc.text("Seeing Conditions: " + this.entry.seeing_conditions, 20, 50, "left");
 
       doc.text("Right Ascention: " + this.entry.right_ascention, 107.95, 30, "left");
-      doc.text("Declination: " + this.entry.declination, 107.95, 40, "left");
-      doc.text("Telescope Type: " + this.entry.telescope_type, 107.95, 50, "left");
-      doc.text("Magnification: " + this.entry.magnification, 107.95, 60, "left");
-      doc.text("Filters: " + this.entry.filters, 107.95, 70, "left");
+      doc.text("Declination: " + this.entry.declination, 107.95, 35, "left");
+      doc.text("Telescope Type: " + this.entry.telescope_type, 107.95, 40, "left");
+      doc.text("Magnification: " + this.entry.magnification, 107.95, 45, "left");
+      doc.text("Filters: " + this.entry.filters, 107.95, 50, "left");
+
+      doc.text("Notes: " + this.entry.notes, 20, 60, "left");
+      var lines = doc.splitTextToSize(loremipsum, 215.9 - 20 - 20);
+      doc.text(20, 20, lines);
 
       doc.save(pdfName + ".pdf");
     },
