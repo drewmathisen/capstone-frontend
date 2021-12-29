12ca529d9d2b95c3ad7939d166fc986e02e984b4
d.mathisen <drew.a.mathisen@gmail.com>
2021-12-28 20:29:27 -0700

    updated search by page to include a functioning search bar and slightly cleaner list of objects

--- src/views/ObservedBodies.vue
+++ src/views/ObservedBodies.vue
@@ -3,31 +3,31 @@
     <header id="header">
       <h1>{{ message }}</h1>
 
-      <ul class="alt">
-        <div v-for="ob in observedBodies">
-          <p>
-            <li>
-              <h2>{{ ob.name }}</h2>
-              <router-link class="button" v-bind:to="`/observedbodies/${ob.id}`">See all entries</router-link>
-            </li>
-          </p>
-        </div>
-      </ul>
+      <form v-on:submit.prevent="submit()">
+        <label>Search:</label>
+        <input type="text" v-model="searchTerm" />
+        <br />
+      </form>
+
+      <div class="row" v-for="ob in observedBodies">
+        <li>
+          <router-link v-bind:to="`/observedbodies/${ob.id}`">{{ ob.name }}</router-link>
+        </li>
+      </div>
     </header>
   </div>
 </template>
 
-<style></style>
-
 <script>
 import axios from "axios";
-
 export default {
   data: function () {
     return {
       message: "All Observed Bodies",
       observedBodies: [],
       searchTerm: "",
+      model: null,
+      result: "",
     };
   },
   created: function () {
@@ -39,8 +39,15 @@ export default {
       axios.get("/observed_bodies").then((response) => {
         // axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
         // localStorage.setItem("jwt", response.data.jwt);
-        console.log(response.data);
         this.observedBodies = response.data;
+      });
+    },
+    submit: function () {
+      console.log(this.observedBodies);
+      console.log(this.searchTerm);
+      axios.get("/bodysearch/" + this.searchTerm).then((response) => {
+        this.result = response.data;
+        this.$router.push("/observedbodies/" + this.result.id);
       });
     },
   },
